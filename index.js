3d465b6cbe9b8e46c45512b412ec04bce674ae38
David Putney <david@davidputney.com>
2017-05-20 08:02:46 -0400

    Cleans up variables. Removes spaces.

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 var postcss = require('postcss');
 
-
-module.exports = postcss.plugin('myplugin', function myplugin(options) {
+module.exports = postcss.plugin('postcss-pixelstorem', function pixelstorem(options) {
     return function (css) {
         options = options || {};
-
         var base = options.base ? options.base : 16;
+
         css.walkRules(function (rule) {
             rule.walkDecls(function (decl, i) {
 
-                var declValue = decl.value;
-                var exclude = options.exclude || [];
+                var declValue = decl.value,
+                    exclude = options.exclude || [];
 
-                var excluded = exclude.some(function(el, i){
+                var excludedTest = exclude.some(function(el, i){
                     return decl.prop === el;
                 });
-                var matches = declValue.match(/\b(em\(\d+\)|rem\(\d+\))/ig, "");
+                var matches = declValue.match(/\b(em\(\d+\)|rem\(\d+\)|\d+px)/ig, "");
 
-                if (matches && !excluded) {
+                if (matches && !excludedTest) {
                   var revised = matches.map(function(el, i) {
-                    var regExp = new RegExp(/\d+/, 'g');
-                    var type = new RegExp(/(^em|rem)/, 'ig');
-
+                    var regExVal = new RegExp(/\d+/, 'g'),
+                        regExType = new RegExp(/(^em|rem|px)/, 'ig'),
+                        unit = regExType.exec(el)[0].toString();
                     var measureType = options.unit
                     ? options.unit
-                    : type.exec(el)[0].toString();
+                    : unit !== 'px' ? unit
+                    : 'rem';
 
-                    var newValue = regExp.exec(el) / base + measureType;
-                    declValue = (declValue.replace(el, newValue));
+                    var convertedVal = regExVal.exec(el) / base + measureType;
+                    declValue = (declValue.replace(el, convertedVal));
                   });
-
                   decl.value = declValue;
-
                 }
             });
         });
