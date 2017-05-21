b425c2d246a6b31a307e7cc5466922fdbe861858
David Putney <david@davidputney.com>
2017-05-21 10:59:57 -0400

    Updates code so that it converts values in media queries. Also, breaks off repeatable sections -- finding matches, converting the values -- into their own functions.

--- index.js
+++ index.js
@@ -5,31 +5,48 @@ module.exports = postcss.plugin('postcss-pixels-to-rem', function pixelstorem(op
         options = options || {};
         var base = options.base ? options.base : 16;
 
+        var mediaQueries = options.mediaQueries === undefined ? true: options.mediaQueries;
+
+        function findMatches (el) {
+          return el.match(/(em\(\d+\)|rem\(\d+\)|\d+px)/ig, "");
+        };
+
+        function convertValues(matches) {
+          var revised = matches.map(function(el, i) {
+            var regExVal = new RegExp(/\d+/, 'g'),
+                regExType = new RegExp(/(^em|rem|px)/, 'ig'),
+                unit = regExType.exec(el)[0].toString();
+            var measureType = options.unit
+            ? options.unit
+            : unit !== 'px' ? unit
+            : 'rem';
+
+            return convertedVal = regExVal.exec(el) / base + measureType;
+          });
+          return revised
+      }
+
         css.walkRules(function (rule) {
+            var ruleParent = rule.parent;
+
+            if (ruleParent.type === "atrule" && ruleParent.name === "media" && mediaQueries) {
+              var matches = findMatches(ruleParent.params);
+              var convertedVal = convertValues(matches);
+              var revisedParam = rule.parent.params;
+              convertedVal.map(function(el, i) {
+                revisedParam = revisedParam.replace(matches[i], el);
+              });
+              rule.parent.params = revisedParam;
+            }
+
             rule.walkDecls(function (decl, i) {
-
-                var declValue = decl.value,
-                    exclude = options.exclude || [];
-
+                var exclude = options.exclude || [];
                 var excludedTest = exclude.some(function(el, i){
                     return decl.prop === el;
                 });
-                var matches = declValue.match(/\b(em\(\d+\)|rem\(\d+\)|\d+px)/ig, "");
-
+                var matches = findMatches(decl.value);
                 if (matches && !excludedTest) {
-                  var revised = matches.map(function(el, i) {
-                    var regExVal = new RegExp(/\d+/, 'g'),
-                        regExType = new RegExp(/(^em|rem|px)/, 'ig'),
-                        unit = regExType.exec(el)[0].toString();
-                    var measureType = options.unit
-                    ? options.unit
-                    : unit !== 'px' ? unit
-                    : 'rem';
-
-                    var convertedVal = regExVal.exec(el) / base + measureType;
-                    declValue = (declValue.replace(el, convertedVal));
-                  });
-                  decl.value = declValue;
+                  decl.value = convertValues(matches);
                 }
             });
         });
